fb346e14847e3b5ec65b37794b73e7935cdf2902
NguyenLe9 <62717480+NguyenLe9@users.noreply.github.com>
2020-10-28 16:14:36 -0400

    Added the frame and basic attributes

--- app/models.py
+++ app/models.py
@@ -54,3 +54,30 @@ class Policy(db.Model):
     death_votes = db.Column(db.Integer)
     adherence_votes = db.Column(db.Integer)
     total_votes = db.Column(db.Integer)
+    
+    citation = db.Column(db.String(200))
+    description = db.Column(db.String(250))
+    tags = db.Column(db.String(15))
+    
+class Vote(db.Model):
+    user_id = db.Column(db.Integer, primary_key=True, db.ForeignKey('user.id'))
+    policy_id_1 = db.Column(db.Integer, primary_key=True, db.ForeignKey('policy.id'))
+    policy_id_2 = db.Column(db.Integer, primary_key=True, db.ForeignKey('policy.id'))
+    
+    economic = db.Column(db.Boolean)
+    death = db.Column(db.Boolean)
+    adherence = db.Column(db.Boolean)
+    conflict = db.Column(db.Boolean)
+   
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    policy_id = db.Column(db.Integer, db.ForeignKey('policy.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    
+    text = db.Column(db.String(100))
+    citation = db.Column(db.String(200))
+    
+    
+    
+    
+    
