69f68086842f946f6ab2dc3f27ef9fdd6c71cfe7
poneill8 <poneill8@gatech.edu>
2021-03-16 11:51:00 -0400

    define spring 2021 database structure

--- app/models.py
+++ app/models.py
@@ -15,14 +15,20 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     admin = db.Column(db.Boolean)
     username = db.Column(db.String(64), index=True, unique=True)
-    name = db.Column(db.String(64))
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
-    contributionPoints = db.Column(db.Integer)
 
-    policies = db.relationship('Policy', backref='user', lazy=True)
-    comments = db.relationship('Comment', backref = 'user', lazy = True)
-    votes = db.relationship('Vote', backref='user', lazy = True)
+    contribution_points = db.Column(db.Integer)
+    reputation = db.Column(db.Integer)
+    reputation_confidence = db.Column(db.Integer)
+
+    streak_length = db.Column(db.Integer)
+    streak_start = db.Column(db.DateTime, default=datetime.utcnow)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+
+    location = db.relationship('Location', backref='user', lazy=True)
+    user_report = db.relationship('UserReport', backref='user', lazy=True)
+    location_log = db.relationship('LocationLog', backref='user', lazy=True)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
@@ -47,44 +53,62 @@ class User(UserMixin, db.Model):
             return
         return User.query.get(id)
 
-class Policy(db.Model):
+class Location(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    title = db.Column(db.String(64))
 
-    economic_votes = db.Column(db.Integer)
-    death_votes = db.Column(db.Integer)
-    adherence_votes = db.Column(db.Integer)
-    total_votes = db.Column(db.Integer)
+    location_name = db.Column(db.String(100))
+    policy_description = db.Column(db.String(200))
+    policy_approved = db.Column(db.Boolean)
 
-    citation = db.Column(db.String(200))
-    description = db.Column(db.String(250))
-    tags = db.Column(db.String(15))
-    policy_votes = db.relationship('PolicyVote', backref='policy', lazy=True)
-    comments = db.relationship('Comment',backref='policy', lazy = True)
+    location_lat = db.Column(db.Float)
+    location_long = db.Column(db.Float)
 
-class PolicyVote(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    policy_id = db.Column(db.Integer, db.ForeignKey('policy.id'))
-    vote_id = db.Column(db.Integer, db.ForeignKey('vote.id'))
+    current_mask_level = db.Column(db.Integer)
+    current_busyness_level = db.Column(db.Integer)
+    last_computed = db.Column(db.DateTime, default=datetime.utcnow)
 
-class Vote(db.Model):
+    average_mask_level = db.Column(db.Integer)
+    average_busyness_level = db.Column(db.Integer)
+
+    user_report = db.relationship('UserReport', backref='location', lazy=True)
+    covid_report = db.relationship('CovidReport', backref='location', lazy=True)
+    location_log = db.relationship('LocationLog', backref='location', lazy=True)
+
+# Note: Only approved comments are visible to public via API
+# Only site admins approve comments, business owners do NOT
+class UserReport(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    policy_id_1 = db.Column(db.Integer, db.ForeignKey('policy.id'))
-    policy_id_2 = db.Column(db.Integer, db.ForeignKey('policy.id'))
+    location_id = db.Column(db.Integer, db.ForeignKey('location.id'))
 
-    policy_votes = db.relationship('PolicyVote', backref='vote', lazy=True)
+    mask_level = db.Column(db.Integer)
+    busyness_level = db.Column(db.Integer)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
 
-    economic = db.Column(db.Boolean)
-    death = db.Column(db.Boolean)
-    adherence = db.Column(db.Boolean)
-    conflict = db.Column(db.Boolean)
+    policy_followed = db.Column(db.Integer)
+    policy_comment = db.Column(db.String(200))
+    policy_comment_approved = db.Column(db.Boolean)
 
-class Comment(db.Model):
+    review_rating = db.Column(db.Integer)
+    review_comment = db.Column(db.String(200))
+    review_comment_approved = db.Column(db.Boolean)
+
+# Privacy concern: Limit CovidReport entries to popular locations (>3 visits)
+# Note: NO direct API access (Doesn't output anything)
+class CovidReports(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    policy_id = db.Column(db.Integer, db.ForeignKey('policy.id'))
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    location_id = db.Column(db.Integer, db.ForeignKey('location.id'))
 
-    text = db.Column(db.String(100))
-    citation = db.Column(db.String(200))
+    # Exposer or exposed at a sepecific location
+
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+    estimated_start = db.Column(db.DateTime, default=datetime.utcnow)
+
+# Note: API only recieves info, no output for any endpoints
+class LocationLog(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    location_id = db.Column(db.Integer, db.ForeignKey('location.id'))
+
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
--- app/routes.py
+++ app/routes.py
@@ -3,7 +3,6 @@ from flask import render_template, flash, redirect, url_for, request
 from flask_login import current_user, login_user, logout_user, login_required
 from werkzeug.urls import url_parse
 from app import app, db
-from app.models import User, Policy, Vote, PolicyVote
 from app.forms import LoginForm, RegistrationForm, EditProfileForm, ResetPasswordRequestForm, ResetPasswordForm, policyForm, RatePolicyForm1, RatePolicyForm2
 
 from app.email import send_password_reset_email
